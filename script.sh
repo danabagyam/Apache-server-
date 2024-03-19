6da56956f458bd90ffa3bf01c9b84d10bfe8c0d2
danabagyam <162982083+danabagyam@users.noreply.github.com>
2024-03-19 09:04:21 -0400

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,3 +1,30 @@
-#!/bin/bash
+#! /bin/bash
 
-sudo dnf install mysql mysql-server -y
+
+#read -p "Enter the service you want to activate: " service
+
+#systemctl=$(systemctl is-active $service)
+
+#if [ $status=="inactive" ]
+#then 
+#	echo "your service is not active"
+#	echo "Done"
+#else 
+#	echo "It is already active"
+#	systemctl enable --now $service
+#fi 
+dnf install httpd -y
+systemctl enable --now httpd
+sleep 5
+
+dnf install mysql mysql-server phpmyadmin -y
+systemctl enable --now mysqld
+sleep 5
+
+firewall-cmd --add-port=80/tcp --permanent
+firewall-cmd --reload 
+mysql -u root -p
+
+grep local /etc/httpd/conf.d/phpmyadmin.conf
+
+sed s/local/"all granted"/g /etc/httpd/conf.d/phpmyadmin.conf
